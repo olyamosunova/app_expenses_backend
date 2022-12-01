cb236a272b2945a493dee06f3efa0e856927c420
Olya Mosunova <vesnushka.ol@mail.ru>
2022-12-01 19:41:32 +0100

    feat(api): add cors

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const http = require('http')
 const path = require('path')
+const cors = require('cors')
 const mongoose = require('mongoose')
 require('dotenv').config()
 
 const app = express()
 
 app.use(express.json({ extended: true }))
+app.use(cors())
 
 app.use('/api/auth', require('./routes/auth.routes'))
 app.use('/api/expense', require('./routes/expense.routes'))
